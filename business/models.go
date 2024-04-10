fa9146f0d5cc9b3b39664deebe7858fa3ce8a5c1
celsopires1999 <celso.pires1999@gmail.com>
2024-04-10 20:13:46 -0300

    course 02_02

--- business/models.go
+++ business/models.go
@@ -14,14 +14,43 @@ type Wind struct {
 	Netto float64
 }
 
-// Print prints the information for a solar product.
-// The string is enriched with the required kineteco legal information.
-func (s *Solar) Print() string {
-	return fmt.Sprintf("%s - %v\n", kinetecoPrint, *s)
+type Energy interface {
+	Solar | Wind
+	Cost() float64
 }
 
-func (w *Wind) Print() string {
-	return fmt.Sprintf("%s - %v\n", kinetecoPrint, *w)
+// Print prints the information for a solar product.
+// The string is enriched with the required kineteco legal information.
+func (s Solar) Print() string {
+	return fmt.Sprintf("%s - %v\n", kinetecoPrint, s)
+}
+
+// Print prints the information for a wind product.
+// The string is enriched with the required kineteco legal information.
+func (w Wind) Print() string {
+	return fmt.Sprintf("%s - %v\n", kinetecoPrint, w)
+}
+
+// PrintGeneric prints the information for a product of type T.
+// T must implement the Energy interface.
+// The string is enriched with the required kineteco legal information.
+func PrintGeneric[T Energy](t T) string {
+	// T must implement the Energy interface
+	// to ensure that it has a Cost method.
+
+	return fmt.Sprintf("%s - %v\n", kinetecoPrint, t)
+}
+
+// PrintSlice prints the information for a slice of energy offers.
+// The information for each offer is printed on a new line,
+// with the index of the offer and the information as returned by
+// PrintGeneric.
+func PrintSlice[T Energy](tt []T) {
+	for i, t := range tt {
+		// Ensure that T implements the Energy interface,
+		// which is required to have a Cost method.
+		fmt.Printf("%d: %s\n", i, PrintGeneric[T](t))
+	}
 }
 
 var kinetecoPrint string = "Kineteco Deal:"
--- main.go
+++ main.go
@@ -17,4 +17,18 @@ func main() {
 	fmt.Println(solar3k.Print())
 	fmt.Println(solar2k.Print())
 	fmt.Println(windWest.Print())
+
+	// With explicit typing
+	fmt.Println(business.PrintGeneric[business.Solar](solar2k))
+	fmt.Println(business.PrintGeneric[business.Wind](windWest))
+
+	// With implicit typing
+	fmt.Println(business.PrintGeneric(solar2k))
+	fmt.Println(business.PrintGeneric(windWest))
+
+	// Print a slice with a variable
+	ss := []business.Solar{solar2k, solar3k}
+	business.PrintSlice(ss)
+	// Print a slice without a variable
+	business.PrintSlice([]business.Wind{windWest, windWest})
 }
